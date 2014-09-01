293d86ed5cc31fbdf5c4a7ad9cfa80a8329fdbec
jnwang <jnwang@eecs.berkeley.edu>
2014-08-31 21:47:45 -0700

    merge dedup into sampleclean 	sbt/sbt-launch-0.13.5.jar 	src/main/scala/sampleclean/clean/dedup/

--- build.sbt
+++ build.sbt
@@ -14,4 +14,16 @@ libraryDependencies += "org.apache.hive" % "hive" % "0.13.1"
 
 libraryDependencies += "javax.servlet" % "javax.servlet-api" % "3.1.0"
 
+
+libraryDependencies ++= Seq(
+    "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" artifacts Artifact("javax.servlet", "jar", "jar"),
+    "org.eclipse.jetty.orbit" % "javax.transaction" % "1.1.1.v201105210645" artifacts Artifact("javax.transaction", "jar", "jar"),
+    "org.eclipse.jetty.orbit" % "javax.mail.glassfish" % "1.4.1.v201005082020" artifacts Artifact("javax.mail.glassfish", "jar", "jar"),
+    "org.eclipse.jetty.orbit" % "javax.activation" % "1.1.0.v201105071233" artifacts Artifact("javax.activation", "jar", "jar")
+    )
+
+
 resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
+
--- src/main/scala/sampleclean/SCDriver.scala
+++ src/main/scala/sampleclean/SCDriver.scala
@@ -8,8 +8,11 @@ import sampleclean.api.SampleCleanContext;
 import sampleclean.api.SampleCleanAQP;
 
 import sampleclean.clean.ParametricOutlier;
-
+import sampleclean.clean.dedup._
 import org.apache.spark.sql.hive.HiveContext
+import sampleclean.clean.dedup.WordTokenizer
+import sampleclean.clean.dedup.BlockingStrategy
+import sampleclean.clean.dedup.BlockingKey
 
 /*This class provides the main driver for the SampleClean
 * application. We execute commands read from the command 
@@ -62,13 +65,25 @@ object SCDriver {
     val hiveContext = new HiveContext(sc);
     val p = new ParametricOutlier(scc);
     //hiveContext.hql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING)")
-    //hiveContext.hql("LOAD DATA LOCAL INPATH 'kv1.txt' INTO TABLE src")
-    scc.closeHiveSession("src_sample")
-    scc.initializeHive("src","src_sample",0.01)
-    println(saqp.rawSCQuery(scc, "src_sample", "key", "count","key > 300", 0.01))
+    //hiveContext.hql("LOAD DATA LOCAL INPATH 'kv1.txt' OVERWRITE INTO TABLE src")
+    hiveContext.hql("CREATE TABLE IF NOT EXISTS dblp (value STRING)")
+    hiveContext.hql("LOAD DATA LOCAL INPATH 'dblp.txt' OVERWRITE INTO TABLE dblp")
+    scc.closeHiveSession("dblp_sample")
+    scc.initializeHive("dblp","dblp_sample",0.001)
+    hiveContext.hql("select count(*) from dblp").collect().foreach(println)
+    hiveContext.hql("select count(*) from dblp_sample_clean").collect().foreach(println)
+
+    //println(saqp.rawSCQuery(scc, "src_sample", "key", "count","key > 300", 0.1))
+    println(saqp.rawSCQuery(scc, "dblp_sample", "value", "count","true", 0.001)) // To Sanjay: predicate cannot be empty
     //p.clean("src_sample", "key", 1)
+
+
+    val d = new Deduplication(scc);
+    val sampleKey = new BlockingKey(Seq(2),WordTokenizer())
+    val fullKey = new BlockingKey(Seq(0),WordTokenizer())
+    d.clean("dblp", "dblp_sample", BlockingStrategy("Jaccard", 0.8, sampleKey, fullKey))
     
-    println(saqp.rawSCQuery(scc, scc.updateTableDuplicateCounts("src_sample", hiveContext.hql("select src_sample_clean.hash as hash, 2 as dup from src_sample_clean limit 10")), "key", "count","key > 300", 0.01))
+    //println(saqp.rawSCQuery(scc, scc.updateTableDuplicateCounts("src_sample", hiveContext.hql("select src_sample_clean.hash as hash, 2 as dup from src_sample_clean limit 10")), "key", "count","key > 300", 0.01))
     /*hiveContext.hql("select * from src_sample_clean").collect().foreach(println)*/
     //scc.filterWriteTable("src_sample", hiveContext.hql("select * from src_sample_clean limit 10"))
     //println(saqp.rawSCQuery(scc, "src_sample_clean", "key", "count","key > 200", 0.01))
