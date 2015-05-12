0a82fb769cbbbdc1cf6a774cc21e4d3d2ce4d716
Daniel Haas <dhaas@eecs.berkeley.edu>
2015-05-12 15:22:12 -0700

    run scripts don't run the python code anymore.

--- scripts/run-all-local.sh
+++ scripts/run-all-local.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# This file runs both the python and the scala code using reasonable defaults
+# This file runs the scala code using reasonable defaults
 # for production assuming a deployment on ec2 using the deploy scripts in the
 # repo.
 #
-# This script assumes that if you have virtualenvwrapper installed, its
-# script is located at /usr/local/bin/virtualenvwrapper.sh. If you aren't using
-# virtualenvs, this script will also work.
-#
 # Usage: ./run-all-local.sh [-s] [MAIN_CLASS]
 # Arguments:
-#   -s: Run the crowd server with ssl
+#   -s: Communicate with AMPCrowd over ssl
 #   MAIN_CLASS: the scala application class to run. If not specified, sbt will
 #     prompt the user to choose.
 
@@ -23,18 +19,6 @@ else
     export SSL=0
     main_class="$1"
 fi
-
-# Activate the virtualenv if it exists.
-if [ -e "/usr/local/bin/virtualenvwrapper.sh" ]
-then
-    source /usr/local/bin/virtualenvwrapper.sh
-    workon sampleclean
-fi
-
-# Start the crowd server
-pushd src/main/python/crowd_server
-./run.sh $@
-popd
 
 # Run the scala code
 if [ -z "$main_class" ]
